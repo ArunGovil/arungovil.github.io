87fca7a812db86666d46d0df3920a572ffdb0f9d
ArunGovil <arun.govil@unifize.com>
2024-10-26 14:41:23 +0530

    Fetch posts

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 // scripts
 
-console.log("Hello World");
+const posts_api =
+  "https://api.github.com/repos/ArunGovil/arungovil.github.io/contents/posts";
+const github_url = "https://github.com/ArunGovil/arungovil.github.io/blob/main";
+
+async function getPosts() {
+  try {
+    const response = await fetch(posts_api);
+    const posts = await response.json();
+    if (posts.length > 0) {
+      const element = document.getElementById("posts");
+      posts.forEach((post) => {
+        const postItem = document.createElement("li");
+        const link = document.createElement("a");
+        link.textContent = post.name;
+        link.href = `${github_url}/${post.path}`;
+        link.target = "_blank";
+        postItem.appendChild(link);
+        element.appendChild(postItem);
+      });
+    }
+  } catch (e) {
+    console.error("Error fetching posts", e);
+  }
+}
+
+getPosts();
